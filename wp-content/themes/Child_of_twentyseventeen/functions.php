317af7d4cf450d14436eea6a52058408e563e793
nikhitsawant <nikhitasawant23@gmail.com>
2019-06-07 14:53:50 -0400

    added googlefont to css

--- wp-content/themes/Child_of_twentyseventeen/functions.php
+++ wp-content/themes/Child_of_twentyseventeen/functions.php
@@ -1,4 +1,9 @@
 <?php
+function wpb_add_google_fonts() {
+	wp_enqueue_style( 'wpb-google-fonts','href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto|Roboto+Condensed&display=swap', false ); 
+}
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
 function my_theme_enqueue_styles() {
 
  $parent_style = 'twentyseventeen-style'; // This is 'twentytseventeen-style' for the Twenty Fifteen theme.
